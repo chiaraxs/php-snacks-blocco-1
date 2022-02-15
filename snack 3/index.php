03de1a9fbcd0adfc93d83535d28759ade0aff9bd
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-15 18:39:23 +0100

    Completed: snack 4

--- snack 3/index.php
+++ snack 3/index.php
@@ -2,3 +2,113 @@
 Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007
 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
 Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z -->
+
+<?php
+
+$posts = [
+
+    '10-01-2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10-02-2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15-05-2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+// var_dump($posts);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 3</title>
+</head>
+
+<body>
+
+    <?php
+
+    foreach ($posts as $k => $v) {
+        echo "<h4>$k</h4>";
+
+        foreach ($v as $post) {
+            echo $post['title'];
+            echo '<br>';
+            echo $post['author'];
+            echo '<br>';
+            echo $post['text'];
+            echo '<br>';
+            echo '<hr>';
+        }
+    }
+    ?>
+
+</body>
+
+</html>
+
+<!-- 
+    k = key
+    v = value 
+-->
+
+<!-- /* foreach example 3: key and value */
+
+$a = array(
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "seventeen" => 17
+);
+
+foreach ($a as $k => $v) {
+    echo "\$a[$k] => $v.\n";
+}
+
+/* foreach example 4: multi-dimensional arrays */
+$a = array();
+$a[0][0] = "a";
+$a[0][1] = "b";
+$a[1][0] = "y";
+$a[1][1] = "z";
+
+foreach ($a as $v1) {
+    foreach ($v1 as $v2) {
+        echo "$v2\n";
+    }
+} -->
